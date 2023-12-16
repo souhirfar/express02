f063c5ca8b3fdeaa8c2c7d069a8b6209d1dd7426
souhirfar <souhir_farjallah@live.fr>
2023-12-16 16:47:04 +0100

    done 02

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 
 const app = express();
-
+app.use(express.json());
 const movieControllers = require("./controllers/movieControllers");
-
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.post("/api/movies", movieControllers.postMovie);
+const userControllers = require("./controllers/userControllers");
+
+app.get("/api/users", userControllers.getUsers);
+
+app.get("/api/users/:id", userControllers.getUserById);
+app.post("/api/users", userControllers.getUsers);
 
 module.exports = app;
