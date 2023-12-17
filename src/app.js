5bcd53c8095e320d5f57d67d74f80f8e6e07068d
souhirfar <souhir_farjallah@live.fr>
2023-12-17 11:52:31 +0100

    updatedone

--- src/app.js
+++ src/app.js
@@ -6,11 +6,13 @@ const movieControllers = require("./controllers/movieControllers");
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies", movieControllers.postMovie);
+app.put("/api/movies/:id", movieControllers.updateMovie);
 const userControllers = require("./controllers/userControllers");
 
 app.get("/api/users", userControllers.getUsers);
 
 app.get("/api/users/:id", userControllers.getUserById);
 app.post("/api/users", userControllers.getUsers);
+app.put("/api/users/:id", userControllers.updateUser);
 
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -46,10 +46,32 @@ database
       res.sendStatus(500);
     });
   };
-
+  const updateMovie = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { title, director, year, color, duration } = req.body;
+  
+    database
+      .query(
+        "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+        [title, director, year, color, duration, id]
+      )
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
 module.exports = {
   getMovies,
   getMovieById,
-  postMovie, // don't forget to export your function ;)
+  postMovie, 
+  updateMovie,
+  
 };
 
